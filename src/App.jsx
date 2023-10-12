3169b7a869df1d66cdee79015cde28a8ff8fa05d
Angel <miguelangel8877@outlook.com>
2023-10-12 11:07:39 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
       <h1 className="text-8xl bg-gradient-to-br from-teal-400 to-purple-900 font-bold text-transparent bg-clip-text">
         Edoce
       </h1>
+      <p>yo</p>
     </div>
   )
 }
