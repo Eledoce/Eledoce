351a118854fba573bef721b5f9e8ad44069dcabc
Angel <miguelangel8877@outlook.com>
2023-10-12 10:25:01 -0600

    vite react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900  h-screen w-screen flex justify-center items-center">
+      <h1 className="text-8xl bg-gradient-to-br from-teal-400 to-purple-900 font-bold text-transparent bg-clip-text">
+        Edoce
+      </h1>
+    </div>
+  )
+}
+
+export default App
