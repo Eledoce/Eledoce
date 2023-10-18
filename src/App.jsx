5f454bb7bcbf5dc6479a61ba255e6b68dec9e2b9
Angel Ramirez <miguelangel8877@outlook.com>
2023-10-17 21:10:33 -0600

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 function App() {
   return (
-    <div className="bg-slate-900  h-screen w-screen flex justify-center items-center">
+    <div className="bg-slate-900  h-screen w-screen  justify-center items-center">
+      <header className="border bg-teal-300 py-2">header</header>
       <h1 className="text-8xl bg-gradient-to-br from-teal-400 to-purple-900 font-bold text-transparent bg-clip-text">
         Edoce
       </h1>
