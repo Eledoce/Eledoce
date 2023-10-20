982fbd3209dff7772662309f73c8a20a6bf6d421
Angel Ramirez <miguelangel8877@outlook.com>
2023-10-20 13:08:15 -0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 function App() {
   return (
-    <div className="bg-slate-900  h-screen w-screen  justify-center items-center">
-      <header className="border bg-teal-300 py-2">header</header>
-      <h1 className="text-8xl bg-gradient-to-br from-teal-400 to-purple-900 font-bold text-transparent bg-clip-text">
-        Edoce
-      </h1>
-      <p>yo</p>
+    <div className="bg-slate-900  h-screen w-screen flex flex-col text-white">
+      <header className="bg-teal-300 p-2">
+        <h1 className="text-4xl bg-gradient-to-br from-teal-400 to-purple-900 font-bold text-transparent bg-clip-text">
+          Edoce
+        </h1>
+      </header>
+      <main className="grow flex md:flex-row flex-col gap-2 p-2">
+        <section className="text-white w-full  border rounded-md">
+          <p>yo, o algo asi</p>
+        </section>
+        <section className="w-full  md:w-3/4 rounded-md border">algo</section>
+      </main>
     </div>
   )
 }
