c204f4712cf7210f0270e56166208c60c0b1423d
Angel Ramirez <miguelangel8877@outlook.com>
2024-01-27 20:13:13 -0600

    add content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
+
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://eledoce.github.io/',
-  base: 'Eledoce',
+  integrations: [tailwind(), react()],
+  site: 'https://edoce.us',
+  // base: 'Eledoce'
 })
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        Baloo: ['Baloo 2 Variable', 'sans-serif'],
+        Palanquin: ['Palanquin Dark', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
