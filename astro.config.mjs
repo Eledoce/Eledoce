6ed5650a4b7c06d04c5f27261b0fb1b75e503afe
Angel Ramirez <miguelangel8877@outlook.com>
2024-05-10 21:18:09 -0600

    update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://edoce.us',
-  // base: 'Eledoce'
+  site: 'https://eledoce.github.io/Eledoce/',
+  base: '/Eledoce',
 })
