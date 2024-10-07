481cfb7bbad5fa68c5d5b385aa63ad326cd25e97
Angel Ramirez <miguelangel8877@outlook.com>
2024-10-06 19:57:30 -0600

    collection blog

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import { BiSolidCopy } from 'react-icons/bi'
 type Props = {
   text: string
 }
@@ -7,9 +6,10 @@ type Props = {
 export default function CopyButton({ text }: Props) {
   return (
     <button
-      className="absolute top-0 right-0 bg-[#44475A] text-[#F8F8F2] px-2 py-1 text-xs font-bold rounded-tr rounded-bl"
+      className="top-0 right-0 bg-[#44475A] text-[#F8F8F2] px-2 py-1 text-xs font-bold rounded-tr rounded-bl"
       onClick={() => navigator.clipboard.writeText(text)}
     >
+      <BiSolidCopy className="inline-block mr-1 mb-1" />
       Copy
     </button>
   )
